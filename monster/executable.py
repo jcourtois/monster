80dc339786e1082b2c9b6dcc289b4cad26627135
James Courtois <jcourtois@thoughtworks.com>
2014-06-24 11:57:33 -0500

    james | adding an option to view monster credentials

--- monster/executable.py
+++ monster/executable.py
@@ -174,6 +174,16 @@ def show(name):
     return deployment
 
 
+def show_testing_credentials(name):
+    """Provides public API and creds to run tempest."""
+    data.load_config(name)
+    deployment = data.load_deployment(name)
+    keystone = deployment.controller(1).local_node.normal['keystone']
+    api = keystone['publicURL']
+    user, password = keystone['user'], keystone['password']
+    return "api: {}\nuser: {}\npassword: {}".format(api, user, password)
+
+
 def explore(name):
     """Explore a deployment in IPython."""
     data.load_config(name)
